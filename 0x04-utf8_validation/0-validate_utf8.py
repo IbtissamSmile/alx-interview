d25ec1e3ed925fc5220a3ae546900fd5c3df9ad6
IbtissamSmile <btissamtoujni@gmail.com>
2024-04-05 22:15:45 +0000

    validation

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,37 +1,32 @@
 #!/usr/bin/python3
 """
-Function that checks if data represents a valid utf8 character
+A method that determines if a given data set represents \
+    a valid UTF-8 encoding.
 """
 
 
 def validUTF8(data):
-    """
-    Function that determines whether the given data is utf-8 compliant.
+    """Determines if a given data set represents a valid UTF-8 encoding.
 
     Args:
-        data (data): Multiple characters represented by a list of integers.
+        data (list): a list of integers
+
     Returns:
-        bool: True if data is a valid UTF-8 integer, otherwise False.
-    """
-    counter = 0
-
-    # Iterate through each integer in the list
-    for num in data:
-        # If counter is 0, check the number of leading 1's present
-        if counter == 0:
-            if (num >> 5) == 0b110:
-                counter = 1
-            elif (num >> 4) == 0b1110:
-                counter = 2
-            elif (num >> 3) == 0b11110:
-                counter = 3
-            elif (num >> 7) != 0:
+        boolean: True if data is a valid UTF-8 encoding
+     """
+    con = 0
+    for i in data:
+        if con == 0:
+            if (i >> 5) == 0b110:
+                con = 1
+            elif (i >> 4) == 0b1110:
+                con = 2
+            elif (i >> 3) == 0b11110:
+                con = 3
+            elif (i >> 7) != 0:
                 return False
-        # If counter is not 0, check if the current integer is a valid byte
         else:
-            if (num >> 6) != 0b10:
+            if (i >> 6) != 0b10:
                 return False
-            counter -= 1
-
-    # A valid counter is 0 after iterating through all the integers
-    return counter == 0
+            con -= 1
+    return con == 0
